a9f8bf0f1b66e45c2b4b31faaaec1596a2b0a8f3
Arve Knudsen <arve.knudsen@gmail.com>
2023-10-23 14:50:19 +0200

    Add tool for reproducing cache poisoning from canceled queries
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

---
+++ tools/repro-cancellation-caching/go.mod
@@ -0,0 +1,3 @@
+module github.com/grafana/mimir/repro-cancellation-caching
+
+go 1.21.3
---
+++ tools/repro-cancellation-caching/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Make query request to Mimir
+	// TODO: Send immediately canceled query request to Mimir
+	// TODO: Send non-canceled query request to Mimir
+	// TODO: Verify success
+	fmt.Printf("OK\n")
+}
